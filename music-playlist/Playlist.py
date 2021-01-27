681d149136a31bfb61f5431a1c3273ad050e5146
Tristan Thompson <tristan15thompson@gmail.com>
2021-01-26 17:54:40 -0800

    [EDIT] added docstrings and cleaned whitespace

--- music-playlist/Playlist.py
+++ music-playlist/Playlist.py
@@ -4,24 +4,19 @@ class Playlist:
   def __init__(self):
     self.__first_song = None
 
-
-  # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
-
   def add_song(self, title):
+    '''Adds song to the Playlist and reassigns the head of the linked list'''
     new_song = Song(title)
     new_song.set_next_song(self.__first_song)
     self.__first_song = new_song
 
-
-
-  # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
-
   def find_song(self, title):
+    '''Finds song in the linked list by traversing the .next of each song/node'''
     counter = 0
     current_song = self.__first_song
 
     while (current_song.get_title().lower() != title.lower()):
-      # if the end of the linked list is reached, return -1
+      # if the end of the linked list is reached, return -1, meaning nothing was found
       if current_song.get_next_song == None:
         return -1
       current_song = current_song.get_next_song()
@@ -29,11 +24,8 @@ class Playlist:
     print(f"Found the song {current_song.get_title()}")
     return counter 
 
-
-
-  # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
-
   def remove_song(self, title):
+    '''Removes the song from the playlist by updating the next node that the previous node points to'''
     current_song = self.__first_song
 
     # this use case for when a linked list has only one entry, or it is at the end of the list
@@ -50,16 +42,10 @@ class Playlist:
         current_song.set_next_song(current_song.get_next_song().get_next_song())
         return
 
-      # this use case is for when the song is the first item in a list
-
-
       current_song = current_song.get_next_song()
 
-
-
-  # TODO: Create a method called length, which returns the number of songs in the playlist.
-
   def length(self):
+    '''Function that returns the length of the Linked List'''
     counter = 0
     current_song = self.__first_song
 
@@ -69,20 +55,12 @@ class Playlist:
 
     return counter
 
-
-  # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
-
-  # Example:
-  # 1. Song Title 1
-  # 2. Song Title 2
-  # 3. Song Title 3
-
   def print_songs(self):
+    '''Dynamically Prints all Songs in the Playlist'''
     current_song = self.__first_song
     counter = 1
+
     while current_song != None:
       print(f'{counter}. {current_song.get_title()}')
       current_song = current_song.get_next_song()
       counter += 1
-
-  
