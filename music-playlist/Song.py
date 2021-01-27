37287dbc92be85fbb3a01ecfe1b42d01101dbc52
Tristan Thompson <tristan15thompson@gmail.com>
2021-01-26 17:56:42 -0800

    [EDIT] added docstrings and cleaned whitespace

--- music-playlist/Song.py
+++ music-playlist/Song.py
@@ -5,35 +5,30 @@ class Song:
       self.__next_song = None
 
 
-  # TODO: Create a getter method for the title attribute, called get_title
   def get_title(self):
+    '''Getter for song title'''
     return self.__title
   
-  
-  # TODO: Create a setter method for the next_song attribute, called set_title. Make sure titles are type cased to strings and are Title Cased.
   def set_title(self, title):
+    '''Setter for song title'''
     title = str(title)
     title = title.title()
     print(title)
     
     self.__next_song = title
 
-
-  # TODO: Create a getter method for the next_song attribute, called get_next_song
   def get_next_song(self):
+    '''Getter for next node/song in the linked list'''
     return self.__next_song
 
-
-  # TODO: Create a setter method for the next_song attribute, called set_next_song
   def set_next_song(self, next_song):
+    '''Setter for the next node//song in the linked list'''
     self.__next_song = next_song
 
-
-  # TODO: Using the __str___ dunder method, return a string of the song title.
   def __str__(self):
+    '''Dunder str method to return the song title'''
     return f'{self.__title}'
 
-
-  # TODO: Using the __repr__ dunder method, return a string formatted as the following:'Song Title -> Next Song Title'
   def __repr__(self):
+    '''Dunder repr method to show the next node that this song points to'''
     return f'{self.__title} -> {self.__next_song}'
