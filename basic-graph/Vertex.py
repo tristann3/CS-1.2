9cdd2a68a428d2eb05a419ddcd330f45f66de4ef
Tristan Thompson <tristan15thompson@gmail.com>
2021-02-25 11:30:40 -0800

    completed Vertex.py class

---
+++ basic-graph/Vertex.py
@@ -0,0 +1,16 @@
+class Vertex:
+  def __inti__(self, id):
+    self.id = id
+    self.neighbors = {}
+
+  def get_id(self):
+    return self.id
+
+  def add_neighbors(self, neighbor_id, weight=0):
+    self.neighbors[neighbor_id] = weight
+
+  def get_neighbors(self):
+    return self.neighbors.keys
+
+  def get_weight(self, neighbor_id):
+    return self.neighbors[neighbor_id]
